d8241e287f0616a38a315c3c8103a14dccf241b2
Alfonso Marquez <marquez.josealfonso@gmail.com>
2022-12-06 17:30:55 +0800

    Add project files.

---
+++ EmployeeProject/Controllers/EmployeesController.cs
@@ -0,0 +1,48 @@
+﻿using EmployeeProject.Data;
+using EmployeeProject.Models;
+using EmployeeProject.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeProject.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeeProjectDbContext employeeProjectDbContext;
+
+        public EmployeesController(EmployeeProjectDbContext employeeProjectDbContext)
+        {
+            this.employeeProjectDbContext = employeeProjectDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employees = await employeeProjectDbContext.Employees.ToListAsync();
+            return View(employees);
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+         public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateofBirth = addEmployeeRequest.DateofBirth
+            };
+
+            await employeeProjectDbContext.Employees.AddAsync(employee);
+            await employeeProjectDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+} 
---
+++ EmployeeProject/Data/EmployeeProjectDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeProject.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeProject.Data
+{
+    public class EmployeeProjectDbContext : DbContext
+    {
+        public EmployeeProjectDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
